3b1f326cdb6e2c34874713fa48928cff2ef64c4e
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2016-09-25 20:45:42 +0100

    Able to handle italics in bold and bold in italics

--- app/Main.hs
+++ app/Main.hs
@@ -1,56 +1,12 @@
 module Main where
 
-import Text.Parsec (parse, try)
-import Text.Parsec.Combinator (many1, count, choice, optional, notFollowedBy, lookAhead)
-import Text.Parsec.Error (ParseError)
-import Text.Parsec.String (Parser)
-import Text.Parsec.Char (letter, char)
-import Control.Applicative ((<*), (<|>), many)
-import Control.Monad (void)
-import Lib
+import Text.Parsec (ParseError)
+import Text.Parsec.Prim (runP)
 
-data Block
-  = Paragraph [Block]
-  | Text String
-  | Emph [Block]
-  | Italic [Block]
-  | Header Int [Block]
-  deriving (Show)
+import Lib (parseBody, Parser)
 
 regularParse :: Parser a -> String -> Either ParseError a
-regularParse p = parse p ""
-
-parseText :: Parser Block
-parseText = do
-  text <- many1 (letter <|> char ' ' <|> try (char '\n' <* notFollowedBy (char '\n') <* lookAhead letter)) 
-  return $ Text (map change text)
-  where change '\n' = ' '
-        change x = x
-
-parseParagraph :: Parser Block
-parseParagraph = do
-  blocks <- many1 $ choice [parseText, parseEmph, parseItalic]
-  void $ many (char '\n')
-  return $ Paragraph blocks
-
-parseEmph :: Parser Block
-parseEmph = do
-  void $ count 2 (char '*')
-  blocks <- many1 $ choice [parseText, try parseItalic]
-  void $ count 2 (char '*')
-  return $ Emph blocks
-
-parseItalic :: Parser Block
-parseItalic = do
-  void $ char '*'
-  blocks <- many1 $ choice [parseText]
-  void $ char '*'
-  return $ Italic blocks
-
-parseBody :: Parser [Block]
-parseBody = do
-  blocks <- many1 $ parseParagraph
-  return blocks
+regularParse p = runP p "" ""
 
 main :: IO ()
 main = do
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,68 @@
 module Lib
-    ( someFunc
-    ) where
+( parseBody
+, Parser
+) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Text.Parsec (parse, try, Parsec)
+import Text.Parsec.Combinator (many1, count, choice, optional, notFollowedBy, lookAhead)
+import Text.Parsec.Error (ParseError)
+import Text.ParserCombinators.Parsec.Prim (getState, setState)
+import Text.Parsec.Char (letter, char)
+import Control.Applicative ((<*), (<|>), many)
+import Control.Monad (void)
+import Debug.Trace (trace)
+
+data Block
+  = Paragraph [Block]
+  | Text String
+  | Emph [Block]
+  | Italic [Block]
+  | Header Int [Block]
+  deriving (Show)
+
+type Parser = Parsec String String
+
+getParsers :: String -> [Parser Block]
+getParsers state = d ++ c ++ [parseText] 
+  where c | 'I' `notElem` state = [try parseItalic]
+          | otherwise = []
+        d | 'E' `notElem` state = [try parseEmph]
+          | otherwise = []
+
+parseText :: Parser Block
+parseText = do
+  text <- many1 (letter <|> char ' ' <|> try (char '\n' <* notFollowedBy (char '\n') <* lookAhead letter)) 
+  return $ Text (map change text)
+  where change '\n' = ' '
+        change x = x
+
+parseParagraph :: Parser Block
+parseParagraph = do
+  blocks <- many1 $ choice [try parseEmph, parseItalic, parseText]
+  void $ many (char '\n')
+  return $ Paragraph blocks
+
+parseEmph :: Parser Block
+parseEmph = do
+  void $ count 2 (char '*')
+  s <- getState
+  setState $ s ++ "E"
+  blocks <- many1 $ choice (getParsers (s ++ "E"))
+  init <$> getState >>= setState
+  void $ count 2 (char '*')
+  return $ Emph blocks
+
+parseItalic :: Parser Block
+parseItalic = do
+  void $ char '*'
+  s <- getState
+  setState $ s ++ "I"
+  blocks <- many1 $ choice (getParsers (s ++ "I"))
+  (init <$> getState) >>= setState
+  void $ char '*'
+  return $ Italic blocks
+
+parseBody :: Parser [Block]
+parseBody = do
+ paras <- many1 $ parseParagraph 
+ return paras
